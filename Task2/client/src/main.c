9e01dd845734a92000e61294214dea62090699be
Alexey-Sagaydak <alex230703@yandex.ru>
2024-07-16 03:04:47 +0700

    add client-server logic

--- Task2/client/src/main.c
+++ Task2/client/src/main.c
@@ -1,238 +1,226 @@
-#include <ncurses.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <mqueue.h>
+#include <sys/types.h>
+#include <sys/msg.h>
+#include <unistd.h>
 #include <time.h>
+#include <signal.h>
+#include <malloc.h>
+#include <sys/ipc.h>
 
+#define REG_QUEUE_NAME "reg_queue"
 #define MAX_NAME_LEN 32
-#define MAX_MSG_LEN 512
-#define MAX_DATE_LEN 64
+#define MAX_MSG_LEN 128
+#define MAX_DATE_LEN 32
+
+typedef enum { CLIENT = 1, SERVER = 2 } SENDER;
+typedef enum {REG = 1, DISCONNECT = 2, MSGINFO = 3, USERINFO = 4 } MSG_TYPE;
 
 typedef struct {
     char username[MAX_NAME_LEN];
     char message[MAX_MSG_LEN];
     char datetime[MAX_DATE_LEN];
-} UserMessage;
+} Message;
 
 typedef struct {
     char username[MAX_NAME_LEN];
+    mqd_t desc;
     int id;
 } User;
 
-UserMessage *messages = NULL;
-int message_count = 0;
+typedef struct {
+    long mtype;
 
-User *users = NULL;
-int user_count = 0;
+    int msg_type;
+    mqd_t desc;
+    int id;
+    char username[MAX_NAME_LEN];
+    char message[MAX_MSG_LEN];
+    char datetime[MAX_DATE_LEN];
+} Msgbuf;
 
-char input_message[MAX_MSG_LEN] = "";
-char current_user[MAX_NAME_LEN] = "";
+User user_info;
 
-WINDOW *msg_win, *user_win, *input_win;
-WINDOW *msg_win_border, *user_win_border, *input_win_border;
-int msg_win_height, msg_win_width;
-int user_win_height, user_win_width;
-int input_win_height, input_win_width;
+User* users;
+int users_size;
 
-int msg_scroll_pos = 0;
-int user_scroll_pos = 0;
-int active_window = 0; // 0 - msg_win, 1 - user_win
+Message* messages;
+int messages_size;
 
-void init_ncurses();
-void create_windows();
-void add_message(const char *username, const char *message);
-void add_user(const char *username);
-void display_messages();
-void display_users();
-void display_input();
-void handle_input();
-void clean_up();
-void demo_data();
-void ask_username();
-void switch_window();
+int reg_queue;
 
-void init_ncurses() {
-    initscr();
-    cbreak();
-    noecho();
-    keypad(stdscr, TRUE);
-    start_color();
-    init_pair(1, COLOR_CYAN, COLOR_BLACK);  // Username
-    init_pair(2, COLOR_GREEN, COLOR_BLACK); // Message
-    init_pair(3, COLOR_YELLOW, COLOR_BLACK); // Datetime
-    init_pair(4, COLOR_WHITE, COLOR_BLACK); // Input
-    init_pair(5, COLOR_WHITE, COLOR_BLACK); // Borders
-}
+void handle_events();
+void cleanup_and_exit();
+void handle_sigint(int sig);
+void handle_events();
+void add_user(User user);
+void add_message(Message message);
 
-void create_windows() {
-    msg_win_height = LINES - 5;
-    msg_win_width = COLS - 24;
-    user_win_height = LINES - 5;
-    user_win_width = 20;
-    input_win_height = 4;
-    input_win_width = COLS;
-
-    msg_win_border = newwin(msg_win_height + 1, msg_win_width + 2, 0, 0);
-    user_win_border = newwin(user_win_height + 1, user_win_width + 2, 0, COLS - user_win_width - 2);
-    input_win_border = newwin(input_win_height, input_win_width, LINES - input_win_height, 0);
-
-    msg_win = newwin(msg_win_height - 1, msg_win_width, 1, 1);
-    user_win = newwin(user_win_height - 1, user_win_width, 1, COLS - user_win_width - 1);
-    input_win = newwin(input_win_height - 2, input_win_width - 2, LINES - input_win_height + 1, 1);
-
-    scrollok(msg_win, TRUE);
-    scrollok(user_win, TRUE);
-    scrollok(input_win, TRUE);
-
-    box(msg_win_border, 0, 0);
-    box(user_win_border, 0, 0);
-    box(input_win_border, 0, 0);
-
-    wrefresh(msg_win_border);
-    wrefresh(user_win_border);
-    wrefresh(input_win_border);
-}
-
-void add_message(const char *username, const char *message) {
-    messages = realloc(messages, sizeof(UserMessage) * (message_count + 1));
-    strncpy(messages[message_count].username, username, MAX_NAME_LEN);
-    strncpy(messages[message_count].message, message, MAX_MSG_LEN);
-
-    time_t now = time(NULL);
-    strftime(messages[message_count].datetime, MAX_DATE_LEN, "%Y-%m-%d %H:%M:%S", localtime(&now));
-    message_count++;
-}
-
-void add_user(const char *username) {
-    users = realloc(users, sizeof(User) * (user_count + 1));
-    strncpy(users[user_count].username, username, MAX_NAME_LEN);
-    users[user_count].id = user_count + 1; // Simple ID assignment for demonstration
-    user_count++;
-}
-
-void display_messages() {
-    werase(msg_win);
-    int start = message_count - msg_scroll_pos - msg_win_height / 2;
-    if (start < 0) start = 0;
-    for (int i = start; i < message_count; i++) {
-        wattron(msg_win, COLOR_PAIR(1));
-        wprintw(msg_win, "%s: ", messages[i].username);
-        wattroff(msg_win, COLOR_PAIR(1));
-        wattron(msg_win, COLOR_PAIR(2));
-        wprintw(msg_win, "%s\n", messages[i].message);
-        wattroff(msg_win, COLOR_PAIR(2));
-        wattron(msg_win, COLOR_PAIR(3));
-        wprintw(msg_win, "%s\n", messages[i].datetime);
-        wattroff(msg_win, COLOR_PAIR(3));
-    }
-    wrefresh(msg_win);
-}
-
-void display_users() {
-    werase(user_win);
-    int start = user_scroll_pos;
-    for (int i = start; i < user_count; i++) {
-        wprintw(user_win, "%s\n", users[i].username);
-    }
-    wrefresh(user_win);
-}
-
-void display_input() {
-    werase(input_win);
-    wattron(input_win, COLOR_PAIR(4));
-    mvwprintw(input_win, 1, 1, "%s", input_message);
-    wattroff(input_win, COLOR_PAIR(4));
-    wrefresh(input_win);
-}
-
-void handle_input() {
-    int ch;
-    int input_index = strlen(input_message);
-
-    while ((ch = wgetch(input_win)) != '\n') {
-        if (ch == 27) { // Escape key
-            clean_up();
-            exit(0);
-        } else if (ch == KEY_BACKSPACE || ch == 127 || ch == '\b') {
-            if (input_index > 0) {
-                input_message[--input_index] = '\0';
-            }
-        } else if (ch == KEY_RESIZE) {
-            create_windows();
-            display_messages();
-            display_users();
-            display_input();
-        } else if (ch == '\t') { // Tab key
-            switch_window();
-        } else if (ch == KEY_UP) {
-            if (active_window == 0 && msg_scroll_pos < message_count - msg_win_height / 2) {
-                msg_scroll_pos++;
-                display_messages();
-            } else if (active_window == 1 && user_scroll_pos < user_count - user_win_height) {
-                user_scroll_pos++;
-                display_users();
-            }
-        } else if (ch == KEY_DOWN) {
-            if (active_window == 0 && msg_scroll_pos > 0) {
-                msg_scroll_pos--;
-                display_messages();
-            } else if (active_window == 1 && user_scroll_pos > 0) {
-                user_scroll_pos--;
-                display_users();
-            }
-        } else if (input_index < MAX_MSG_LEN - 1) {
-            input_message[input_index++] = ch;
-            input_message[input_index] = '\0';
-        }
-        display_input();
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <client_name>\n", argv[0]);
+        exit(1);
     }
 
-    if (input_index > 0) {
-        add_message(current_user, input_message);
-        input_message[0] = '\0';
-        display_messages();
-        display_input();
+    signal(SIGINT, handle_sigint);
+
+    Msgbuf reg_message, resp;
+    key_t reg_key = ftok(REG_QUEUE_NAME, 10);
+
+    if (reg_key == -1) {
+        perror("ftok");
+        exit(1);
     }
+
+    reg_queue = msgget(reg_key, 0666 | IPC_CREAT);
+    if (reg_queue == -1) {
+        perror("msgget");
+        exit(1);
+    }
+
+    printf("Client started on desc = %d...\n", reg_queue);
+
+    reg_message.mtype = CLIENT;
+    reg_message.msg_type = REG;
+    strcpy(reg_message.username, argv[1]);
+    reg_message.id = getpid();
+
+    if (msgsnd(reg_queue, &reg_message, sizeof(Msgbuf) - sizeof(long), 0) == -1) {
+        perror("msgsnd");
+        exit(1);
+    }
+
+    if (msgrcv(reg_queue, &resp, sizeof(Msgbuf) - sizeof(long), SERVER, 0) < 0) {
+        perror("msgrcv");
+        exit(1);
+    }
+
+    user_info.desc = resp.desc;
+    user_info.id = resp.id;
+    strcpy(user_info.username, resp.username);
+
+    printf("Registered with username %s on desc %d\n", user_info.username, user_info.desc);
+
+    handle_events();
+
+    cleanup_and_exit();
+
+    return 0;
 }
 
-void clean_up() {
-    free(messages);
-    free(users);
-    endwin();
+void cleanup_and_exit() {
+    Msgbuf exit_message;
+
+    strcpy(exit_message.username, user_info.username);
+    exit_message.mtype = CLIENT;
+    exit_message.msg_type = DISCONNECT;
+    exit_message.id = user_info.id;
+    exit_message.desc = user_info.desc;
+
+    if (msgsnd(reg_queue, &exit_message, sizeof(Msgbuf) - sizeof(long), 0) == -1) {
+        perror("msgsnd");
+        exit(1);
+    }
+
+    printf("Exiting...\n");
+    exit(0);
 }
 
-void demo_data() {
-    add_user("Alice");
-    add_user("Bob");
-    add_user("Charlie");
-    add_message("Alice", "Hello, world!");
-    add_message("Bob", "Hi, Alice!");
-    add_message("Charlie", "Good morning!");
+void handle_sigint(int sig) {
+    cleanup_and_exit();
 }
 
-void ask_username() {
-    echo();
-    mvprintw(LINES / 2, (COLS - 20) / 2, "Enter your name: ");
-    getnstr(current_user, MAX_NAME_LEN - 1);
-    noecho();
-}
-
-void switch_window() {
-    active_window = (active_window + 1) % 2;
-}
-
-int main() {
-    init_ncurses();
-    ask_username();
-    create_windows();
-    demo_data();
-    display_messages();
-    display_users();
-    display_input();
+void handle_events(){
+    Msgbuf msg;
+    User user;
+    Message message;
 
     while (1) {
-        handle_input();
+        if (msgrcv(user_info.desc, &msg, sizeof(Msgbuf) - sizeof(long), SERVER, IPC_NOWAIT) >= 0) {
+            switch (msg.msg_type) {
+                case MSGINFO:
+                    printf("new msg: %s\n", msg.username);
+
+                    strcpy(message.username, msg.username);
+                    strcpy(message.message, msg.message);
+                    strcpy(message.datetime, msg.datetime);
+
+                    add_message(message);
+                    break;
+                case USERINFO:
+                    printf("new user: %s\n", msg.username);
+
+                    strcpy(user.username, msg.username);
+                    user.id = msg.id;
+                    user.desc = msg.desc;
+
+                    add_user(user);
+                    break;
+                case DISCONNECT:
+                    printf("user %s disconnected\n", msg.username);
+                    remove_user(msg.id);
+                    break;
+            }
+        }
+    }
+}
+
+void add_user(User user) {
+  User* temp = (User*)realloc(users, (++users_size) * sizeof(User));
+  if (temp != NULL || (temp == NULL && users_size == 0)) {
+    users = temp;
+  } else {
+    perror("realloc");
+    free(users);
+    users = NULL;
+  }
+
+  strcpy(users[users_size - 1].username, user.username);
+  users[users_size - 1].desc = user.desc;
+  users[users_size - 1].id = user.id;
+}
+
+void add_message(Message message) {
+  Message* temp = (Message*)realloc(messages, (++messages_size) * sizeof(Message));
+  if (temp != NULL || (temp == NULL && messages_size == 0)) {
+    messages = temp;
+  } else {
+    perror("realloc");
+    free(messages);
+    users = NULL;
+  }
+
+  strcpy(messages[messages_size - 1].username, message.username);
+  strcpy(messages[messages_size - 1].message, message.message);
+  strcpy(messages[messages_size - 1].datetime, message.datetime);
+}
+
+void remove_user(int id) {
+    int index = -1;
+    for (int i = 0; i < users_size; ++i) {
+        if (users[i].id == id) {
+            index = i;
+            break;
+        }
     }
 
-    clean_up();
-    return 0;
+    if (index == -1) {
+        perror("User not found");
+        return;
+    }
+
+    for (int i = index; i < users_size - 1; ++i) {
+        users[i] = users[i + 1];
+    }
+
+    User* temp = (User*)realloc(users, (--users_size) * sizeof(User));
+    if (temp != NULL || users_size == 0) {
+        users = temp;
+    } else {
+        perror("realloc");
+        free(users);
+        users = NULL;
+    }
 }
--- Task2/server/src/main.c
+++ Task2/server/src/main.c
@@ -1,104 +1,325 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <sys/ipc.h>
+#include <mqueue.h>
+#include <sys/types.h>
 #include <sys/msg.h>
+#include <unistd.h>
 #include <time.h>
+#include <signal.h>
+#include <malloc.h>
+#include <sys/ipc.h>
 #include <errno.h>
 
+#define REG_QUEUE_NAME "reg_queue"
 #define MAX_NAME_LEN 32
-#define MAX_MSG_LEN 512
-#define MAX_DATE_LEN 64
-#define SERVER_QUEUE_KEY 1234
+#define MAX_MSG_LEN 128
+#define MAX_DATE_LEN 32
+
+typedef enum { CLIENT = 1, SERVER = 2 } SENDER;
+typedef enum {REG = 1, DISCONNECT = 2, MSGINFO = 3, USERINFO = 4 } MSG_TYPE;
 
 typedef struct {
-    long msg_type;
     char username[MAX_NAME_LEN];
     char message[MAX_MSG_LEN];
     char datetime[MAX_DATE_LEN];
-} UserMessage;
+} Message;
 
 typedef struct {
     char username[MAX_NAME_LEN];
-    int queue_id;
+    mqd_t desc;
+    int id;
 } User;
 
 typedef struct {
-    long msg_type;
+    long mtype;
+
+    int msg_type;
+    mqd_t desc;
+    int id;
     char username[MAX_NAME_LEN];
-} UserConnectMessage;
+    char message[MAX_MSG_LEN];
+    char datetime[MAX_DATE_LEN];
+} Msgbuf;
 
-User *users = NULL;
-int user_count = 0;
-int server_queue_id;
+User* users;
+int users_size;
 
-pthread_mutex_t user_mutex = PTHREAD_MUTEX_INITIALIZER;
+Message* messages;
+int messages_size;
 
-void add_user(const char *username, int queue_id) {
-    pthread_mutex_lock(&user_mutex);
-    users = realloc(users, sizeof(User) * (user_count + 1));
-    strncpy(users[user_count].username, username, MAX_NAME_LEN);
-    users[user_count].queue_id = queue_id;
-    user_count++;
-    pthread_mutex_unlock(&user_mutex);
-}
+int reg_queue;
+int id;
 
-void send_message_to_all(UserMessage *msg) {
-    pthread_mutex_lock(&user_mutex);
-    for (int i = 0; i < user_count; i++) {
-        msgsnd(users[i].queue_id, msg, sizeof(UserMessage) - sizeof(long), 0);
-    }
-    pthread_mutex_unlock(&user_mutex);
-}
-
-void *client_handler(void *arg) {
-    int client_queue_id = *((int *)arg);
-    free(arg);
-
-    UserMessage msg;
-    while (1) {
-        if (msgrcv(client_queue_id, &msg, sizeof(UserMessage) - sizeof(long), 0, 0) >= 0) {
-            send_message_to_all(&msg);
-        }
-    }
-}
+void handle_events();
+void cleanup_and_exit();
+void handle_sigint(int sig);
+void create_file(const char * filename);
+int register_user(User user);
+void add_user(User user);
+void demo_data();
+void send_data_to_new_user(User user);
+void add_message(Message msg);
+void disconnect_user(User user);
+void remove_user(int id);
 
 int main() {
-    server_queue_id = msgget(SERVER_QUEUE_KEY, IPC_CREAT | 0666);
-    if (server_queue_id == -1) {
-        perror("msgget");
-        exit(EXIT_FAILURE);
+    signal(SIGINT, handle_sigint);
+    demo_data();
+
+    create_file(REG_QUEUE_NAME);
+    key_t reg_key = ftok(REG_QUEUE_NAME, 10);
+    if (reg_key == -1) {
+        perror("ftok");
+        exit(1);
     }
 
-    while (1) {
-        UserConnectMessage connect_msg;
-        if (msgrcv(server_queue_id, &connect_msg, sizeof(UserConnectMessage) - sizeof(long), 1, 0) >= 0) {
-            int client_queue_id = msgget(connect_msg.username[0], IPC_CREAT | 0666);
-            if (client_queue_id == -1) {
-                perror("msgget");
-                continue;
-            }
+    reg_queue = msgget(reg_key, 0666 | IPC_CREAT);
+    if (reg_queue == -1) {
+        perror("msgget");
+        exit(1);
+    }
 
-            add_user(connect_msg.username, client_queue_id);
+    printf("Server started and waiting for client registrations on desc = %d...\n", reg_queue);
 
-            UserMessage welcome_msg;
-            welcome_msg.msg_type = 1;
-            strncpy(welcome_msg.username, "Server", MAX_NAME_LEN);
-            snprintf(welcome_msg.message, MAX_MSG_LEN, "%s has joined the chat.", connect_msg.username);
-            time_t now = time(NULL);
-            strftime(welcome_msg.datetime, MAX_DATE_LEN, "%Y-%m-%d %H:%M:%S", localtime(&now));
+    handle_events();
 
-            send_message_to_all(&welcome_msg);
+    cleanup_and_exit();
 
-            int *arg = malloc(sizeof(int));
-            *arg = client_queue_id;
-            pthread_t thread;
-            pthread_create(&thread, NULL, client_handler, arg);
-            pthread_detach(thread);
+    return 0;
+}
+
+void demo_data(){
+    Message m1, m2, m3;
+    
+    strcpy(m1.username, "user1");
+    strcpy(m1.message, "msg1");
+    strcpy(m1.datetime, "15.07.2024");
+    
+    strcpy(m2.username, "user2");
+    strcpy(m2.message, "msg2");
+    strcpy(m2.datetime, "16.07.2024");
+
+    strcpy(m3.username, "user3");
+    strcpy(m3.message, "msg3");
+    strcpy(m3.datetime, "17.07.2024");
+
+    add_message(m1);
+    add_message(m2);
+    add_message(m3);
+}
+
+void remove_user(int id) {
+    int index = -1;
+    for (int i = 0; i < users_size; ++i) {
+        if (users[i].id == id) {
+            index = i;
+            break;
         }
     }
 
-    return 0;
+    if (index == -1) {
+        perror("User not found");
+        return;
+    }
+
+    for (int i = index; i < users_size - 1; ++i) {
+        users[i] = users[i + 1];
+    }
+
+    User* temp = (User*)realloc(users, (--users_size) * sizeof(User));
+    if (temp != NULL || users_size == 0) {
+        users = temp;
+    } else {
+        perror("realloc");
+        free(users);
+        users = NULL;
+    }
+}
+
+void add_user(User user) {
+  User* temp = (User*)realloc(users, (++users_size) * sizeof(User));
+  if (temp != NULL || (temp == NULL && users_size == 0)) {
+    users = temp;
+  } else {
+    perror("realloc");
+    free(users);
+    users = NULL;
+  }
+
+  strcpy(users[users_size - 1].username, user.username);
+  users[users_size - 1].desc = user.desc;
+  users[users_size - 1].id = user.id;
+}
+
+void add_message(Message message) {
+  Message* temp = (Message*)realloc(messages, (++messages_size) * sizeof(Message));
+  if (temp != NULL || (temp == NULL && messages_size == 0)) {
+    messages = temp;
+  } else {
+    perror("realloc");
+    free(messages);
+    users = NULL;
+  }
+
+  strcpy(messages[messages_size - 1].username, message.username);
+  strcpy(messages[messages_size - 1].message, message.message);
+  strcpy(messages[messages_size - 1].datetime, message.datetime);
+}
+
+void handle_events() {
+    Msgbuf msg;
+    User user;
+
+    while (1) {
+        if (msgrcv(reg_queue, &msg, sizeof(Msgbuf) - sizeof(long), CLIENT, IPC_NOWAIT) >= 0) {
+            switch (msg.msg_type) {
+                case REG:
+                    printf("User %s asks for registration.\n", msg.username);
+                    strcpy(user.username, msg.username);
+                    register_user(user);
+                    break;
+                case DISCONNECT:
+                    printf("User %s has disconnected.\n", msg.username);
+                    strcpy(user.username, msg.username);
+                    user.id = msg.id;
+                    user.desc = msg.desc;
+                    disconnect_user(user);
+                    break;
+            }
+        }
+        for (int i = 0; i < users_size; i++){
+            if (msgrcv(users[i].desc, &msg, sizeof(Msgbuf) - sizeof(long), CLIENT, IPC_NOWAIT) >= 0) {
+            switch (msg.msg_type) {
+                case MSGINFO:
+                    printf("Received message from %s: %s\n", msg.username, msg.message);
+                    break;
+                case USERINFO:
+                    printf("User %s has joined the chat.\n", msg.username);
+                    break;
+                }
+            }
+        }
+    }
+}
+
+void send_broadcast(Msgbuf buf, int exeption_id){
+    for (int i = 0; i < users_size; i++){
+        if (users[i].id == exeption_id){
+            continue;
+        }
+
+        if (msgsnd(users[i].desc, &buf, sizeof(Msgbuf) - sizeof(long), 0) == -1) {
+            perror("msgsnd");
+            exit(1);
+        }
+    }
+}
+
+void disconnect_user(User user){
+    Msgbuf buf;
+
+    buf.mtype = SERVER;
+    buf.msg_type = DISCONNECT;
+    buf.id = user.id;
+    buf.desc = user.desc;
+    strcpy(buf.username, user.username);
+
+    remove_user(user.id);
+    send_broadcast(buf, -1);
+}
+
+int register_user(User user){
+    Msgbuf resp;
+
+    user.id = id++;
+    char filename[20] = "";
+    sprintf(filename, "user_%d", user.id);
+    create_file(filename);
+
+    key_t key = ftok(filename, 10);
+    if (key == -1) {
+        perror("ftok");
+        exit(1);
+    }
+
+    user.desc = msgget(key, 0666 | IPC_CREAT);
+    if (user.desc == -1) {
+        perror("msgget");
+        exit(1);
+    }
+
+    add_user(user);
+
+    resp.desc = user.desc;
+    resp.mtype = SERVER;
+    resp.msg_type = USERINFO;
+    resp.id = user.id;
+    strcpy(resp.username, user.username);
+
+
+    if (msgsnd(reg_queue, &resp, sizeof(Msgbuf) - sizeof(long), 0) == -1) {
+        perror("msgsnd");
+        exit(1);
+    }
+
+    send_data_to_new_user(users[users_size - 1]);
+    send_broadcast(resp, user.id);
+
+    return user.desc;
+}
+
+void send_data_to_new_user(User user){
+    Msgbuf resp;
+
+    for (int i = 0; i < messages_size; i++) {
+        strcpy(resp.message, messages[i].message);
+        strcpy(resp.username, messages[i].username);
+        strcpy(resp.datetime, messages[i].datetime);
+        resp.mtype = SERVER;
+        resp.msg_type = MSGINFO;
+
+        if (msgsnd(user.desc, &resp, sizeof(Msgbuf) - sizeof(long), 0) == -1) {
+            perror("msgsnd");
+            exit(1);
+        }
+    }
+
+    for (int i = 0; i < users_size; i++) {
+        strcpy(resp.username, users[i].username);
+        resp.id = users[i].id;
+        resp.desc = users[i].desc;
+        resp.mtype = SERVER;
+        resp.msg_type = USERINFO;
+
+        if (msgsnd(user.desc, &resp, sizeof(Msgbuf) - sizeof(long), 0) == -1) {
+            perror("msgsnd");
+            exit(1);
+        }
+    }
+}
+
+void cleanup_and_exit() {
+    if (msgctl(reg_queue, IPC_RMID, NULL) == -1) {
+        perror("msgctl");
+        exit(1);
+    }
+
+    printf("Server stopped. Cleaning up and exiting...\n");
+    exit(0);
+}
+
+void create_file(const char * filename){
+    if (access(filename, F_OK) == -1) {
+        int fd = open(filename, O_CREAT | O_EXCL, 0666);
+        if (fd == -1) {
+            perror("open");
+            exit(1);
+        }
+        close(fd);
+    }
+}
+
+void handle_sigint(int sig) {
+    cleanup_and_exit();
 }
